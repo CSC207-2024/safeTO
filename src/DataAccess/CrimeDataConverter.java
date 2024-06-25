aa6d3d2a4059cf5e334f98cb12122ef151c10e45
Bilin22 <nongbilin1022@outlook.com>
2024-06-25 12:26:46 -0400

    add CrimeDataConverter

---
+++ src/DataAccess/CrimeDataConverter.java
@@ -0,0 +1,96 @@
+package src.DataAccess;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.io.csv.CsvReadOptions;
+import java.io.StringReader;
+
+/**
+ * A class that is responsible for converting input data into other formats.
+ */
+public class CrimeDataConverter {
+
+    private final String[] jsonKeys = {
+            "EVENT_UNIQUE_ID", "OCC_YEAR", "OCC_MONTH", "OCC_DAY", "OCC_DOW", "OCC_HOUR",
+            "LAT_WGS84", "LONG_WGS84", "NEIGHBOURHOOD_158", "HOOD_158", "PREMISES_TYPE", "MCI_CATEGORY"
+    };
+
+    /**
+     * Converts a JSONArray of JSONObjects into a CSV-formatted StringBuilder.
+     * Each JSONObject in the JSONArray has a nested JSONObject under the "attributes" key
+     * which contains the actual data to be converted into CSV format.
+     *
+     * @param data The JSONArray to be converted into CSV format.
+     * @return StringBuilder containing the CSV formatted data.
+     * @throws JSONException If an error occurs during the parsing of the JSON data.
+     */
+    public StringBuilder jsonToString(JSONArray data) throws JSONException {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.join(",", jsonKeys)).append("\n");
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject obj = data.getJSONObject(i).getJSONObject("attributes");
+            appendRow(builder, obj);
+        }
+        return builder;
+    }
+
+    /**
+     * A private helper method which appends a row to the StringBuilder in CSV format.
+     *
+     * @param builder The StringBuilder to which the CSV row will be appended.
+     * @param obj The JSONObject containing the data to be appended as a CSV row.
+     */
+    private void appendRow(StringBuilder builder, JSONObject obj) {
+        for (String key : jsonKeys) {
+            builder.append(obj.optString(key, "")).append(",");
+        }
+        builder.setLength(builder.length() - 1);
+        builder.append("\n");
+    }
+
+    /**
+     * Converts a StringBuilder CSV formatted StringBuilder into a Tablesaw Table.
+     *
+     * @param builderData The StringBuilder containing the CSV data.
+     * @return a Table containing the data represented by the StringBuilder.
+     */
+    public Table stringBuilderToTable(StringBuilder builderData) {
+
+        StringReader reader = new StringReader(builderData.toString());
+
+        CsvReadOptions options = CsvReadOptions.builder(reader)
+                .header(true)
+                .separator(',')
+                .build();
+
+        try {
+            return Table.read().usingOptions(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+//    public static void main(String[] args) throws JSONException {
+//        CrimeDataFetcher fetcher = new CrimeDataFetcher();
+//        JSONArray data = fetcher.fetchData();
+//        CrimeDataConverter converter = new CrimeDataConverter();
+//        StringBuilder out = converter.jsonToString(data);
+//        Table t = converter.stringBuilderToTable(out);
+//        System.out.println(t.structure());
+//        System.out.println(t.first(10));
+    }
+
+
+
+
+
+
+
+
+
+
