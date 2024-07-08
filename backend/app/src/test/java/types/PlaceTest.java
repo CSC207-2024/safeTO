0f0e76c46d1fa281d3db71242eb74e6943770bbb
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-07-08 18:43:41 -0400

    test(backend/types): add test cases for methods of the Place class

--- backend/app/src/test/java/types/PlaceTest.java
+++ backend/app/src/test/java/types/PlaceTest.java
@@ -1,5 +1,6 @@
 package types;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.BeforeAll;
@@ -16,13 +17,16 @@ public class PlaceTest {
     private static final Gson gson = new Gson();
 
     private static final Place place = gson.fromJson(placeJson, Place.class);
+    private static final Place _place = gson.fromJson(placeJson, Place.class);
     private static final Address address = gson.fromJson(addressJson, Address.class);
 
     @Test
+    void testEquals() {
+        assertEquals(place, _place);
+    }
+
+    @Test
     void testGetAddress() {
-        // System.out.println(gson.toJson(address));
-        // System.out.println(gson.toJson(place.getAddress()));
-        // assertEquals(address.hashCode(), place.getAddress().hashCode());
         assertEquals(address, place.getAddress());
     }
 
@@ -33,66 +37,69 @@ public class PlaceTest {
 
     @Test
     void testGetBoundingbox() {
-
+        assertArrayEquals(new String[] { "43.6666818", "43.6667634", "-79.3921205", "-79.3919253" },
+                place.getBoundingbox());
     }
 
     @Test
     void testGetClassType() {
-
+        assertEquals("leisure", place.getClassType());
     }
 
     @Test
     void testGetDisplayName() {
-
+        assertEquals(
+                "Queen's Park Crescent East, Bloor Street Culture Corridor, University—Rosedale, Old Toronto, Toronto, Golden Horseshoe, Ontario, M5S 1K7, Canada",
+                place.getDisplayName());
     }
 
     @Test
     void testGetImportance() {
-
+        assertEquals(9.99999999995449e-06, place.getImportance());
     }
 
     @Test
     void testGetLat() {
-
+        assertEquals("43.66674125", place.getLat());
     }
 
     @Test
     void testGetLicence() {
-
+        assertEquals("Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright", place.getLicence());
     }
 
     @Test
     void testGetLon() {
-
+        assertEquals("-79.39198131930586", place.getLon());
     }
 
     @Test
     void testGetName() {
-
+        assertEquals("", place.getName());
     }
 
     @Test
     void testGetOsmId() {
-
+        assertEquals(1004829152, place.getOsmId());
     }
 
     @Test
     void testGetOsmType() {
-
+        assertEquals("way", place.getOsmType());
     }
 
     @Test
     void testGetPlaceId() {
-
+        assertEquals(7937679, place.getPlaceId());
     }
 
     @Test
     void testGetPlaceRank() {
-
+        assertEquals(30, place.getPlaceRank());
     }
 
     @Test
     void testGetType() {
-
+        assertEquals("garden", place.getType());
     }
 }
