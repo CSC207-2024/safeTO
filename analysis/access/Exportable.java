8a035a3bb8ac1d0631c753fa5a47f35b0f2d83e8
Bilin22 <nongbilin1022@outlook.com>
2024-07-08 22:51:28 -0400

    feat(analysis/access): add exportable interface

---
+++ analysis/access/Exportable.java
@@ -0,0 +1,12 @@
+package analysis.access;
+
+import tech.tablesaw.api.Table;
+import org.jfree.chart.JFreeChart;
+
+/**
+ * An interface for exporting objects.
+ */
+public interface Exportable {
+    void writeToJson(Table table, String outputPath);
+    void exportToSVG(JFreeChart chart, String outputPath);
+}
