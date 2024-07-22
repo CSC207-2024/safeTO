7bb24f5c16ab8b59f334bbb136cb3b381e298e3c
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-07-21 21:40:46 -0400

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit 5c0c36215a8378ab8058757e7f0346df1f6c9af2, reversing
    changes made to 4415356049bd1523b4eeaad0b19e14af78ecab5a.

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,20 +1,29 @@
 import React, { useImperativeHandle, forwardRef, useEffect, useState, useRef } from 'react';
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
-// import L from 'leaflet';
+import { MapContainer, TileLayer, GeoJSON, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'esri-leaflet/dist/esri-leaflet';
-// import geoJsonData from '../../frontend/public/Toronto_Neighbourhoods.geojson'; // Import GeoJSON file
-// import axios from 'axios';
 
+// Component to track mouse movements and update coordinates
+const HoverCoordinates = ({ setCoordinates }) => {
+  useMapEvents({
+    mousemove: (event) => {
+      const { lat, lng } = event.latlng;
+      setCoordinates({ lat, lng }); // Update coordinates state
+    },
+  });
+  return null;
+};
 
+// Map component with forwardRef to allow parent components to control the map
 const Map = forwardRef((props, ref) => {
-  // Central View Coordinates of Toronto
-  const position = [43.737207, -79.343448];
+  const { setCoordinates } = props;
+  const position = [43.737207, -79.343448]; // Initial position for the map (Toronto)
+  const mapRef = useRef(); // Reference to the map instance
 
-  const mapRef = useRef();
-
+  // State for storing GeoJSON data
   const [geoJsonData, setGeoJsonData] = useState(null);
 
+  // Expose map methods to parent components
   useImperativeHandle(ref, () => ({
     flyTo: (coords, zoom) => {
       if (mapRef.current) {
@@ -28,78 +37,63 @@ const Map = forwardRef((props, ref) => {
     }
   }));
 
+  // Fetch GeoJSON data and set initial view
   useEffect(() => {
-    // Fetch the GeoJSON data for Toronto
-    fetch('/data/Toronto_Neighbourhoods.geojson')
-      .then((response) => response.json())
-      .then((data) => setGeoJsonData(data))
-      .catch((error) => console.error('Error fetching GeoJSON data:', error));
+    fetch('../../frontend/public/Toronto_Neighbourhoods.geojson')
+        .then((response) => response.json())
+        .then((data) => setGeoJsonData(data)) // Set GeoJSON data
+        .catch((error) => console.error('Error fetching GeoJSON data:', error));
     if (mapRef.current) {
-      mapRef.current.setView([43.651070, -79.347015], 13); // Central coordinate of Toronto
+      mapRef.current.setView([43.651070, -79.347015], 16); // Set initial view to Toronto with zoom level 16
     }
   }, []);
 
+  // Styles for GeoJSON features
   const highlightStyle = {
     weight: 2,
-    color: '#00FF00', // Outline color when hovered
+    color: '#00FF00',
     dashArray: '',
     fillOpacity: 0.7,
-    fillColor: '#00FF00' // Fill color when hovered
+    fillColor: '#00FF00'
   };
 
   const defaultStyle = {
     weight: 2,
     opacity: 0.5,
-    color: '#3388ff', // Outline color
+    color: '#3388ff',
     dashArray: '3',
     fillOpacity: 0.1,
-    fillColor: '#3388ff' // Fill color
+    fillColor: '#3388ff'
   };
 
-
-  //   <MapContainer center={position} zoom={12} style={{ height: '100vh', width: '100%' }}>
-
-  //     {/* <Marker position={position}>
-  //       <Popup>
-  //         Toronto Center view
-  //       </Popup>
-  //     </Marker> */}
-
-
-
-  // );
-
+  // Function to apply styles and event handlers to each GeoJSON feature
   const onEachArea = (area, layer) => {
-    // const name = area.properties.name;
     layer.on({
       mouseover: (event) => {
-        event.target.setStyle(highlightStyle);
+        event.target.setStyle(highlightStyle); // Highlight on hover
       },
       mouseout: (event) => {
-        event.target.setStyle(defaultStyle);
+        event.target.setStyle(defaultStyle); // Reset style on mouseout
       }
     });
   };
 
   return (
-    <MapContainer center={position} zoom={12} style={{ height: '100vh', width: '100%' }}>
-      {/* <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      /> */}
-      <TileLayer
-        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
-        attribution='&copy; <a href="https://www.esri.com/">Esri</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        opacity={0.6}
-
-      />
-
-      {geoJsonData && (
-        <GeoJSON data={geoJsonData} style={highlightStyle} onEachFeature={onEachArea} />
-      )}
-    </MapContainer>
+      <MapContainer center={position} zoom={15} style={{ height: '100vh', width: '100%' }} ref={mapRef}>
+        {/* Tile layer for the map */}
+        <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+            attribution='&copy; <a href="https://www.esri.com/">Esri</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            opacity={0.6}
+        />
+        {/* Render GeoJSON data if available */}
+        {geoJsonData && (
+            <GeoJSON data={geoJsonData} style={defaultStyle} onEachFeature={onEachArea} />
+        )}
+        {/* Component to track mouse movements */}
+        <HoverCoordinates setCoordinates={setCoordinates} />
+      </MapContainer>
   );
 });
-
 
 export default Map;
