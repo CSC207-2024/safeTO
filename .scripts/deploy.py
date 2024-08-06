74f36fc634f271d4e574e217ef99685b38b5d7fe
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-08-05 20:39:21 -0400

    fix(.scripts/deploy): minor fixes

--- .scripts/deploy.py
+++ .scripts/deploy.py
@@ -6,27 +6,34 @@ from datetime import datetime
 current_dir = os.path.dirname(os.path.abspath(__file__))
 root_dir = os.path.dirname(current_dir)
 backend_dir = os.path.join(root_dir, "backend")
+
+is_my_laptop = os.environ.get("COMPUTERNAME") == "ARKG15"
+
 war_path = os.path.join(backend_dir, "app", "build", "libs", "ROOT.war")
-tomcat_path = os.getenv("SAFETO_BINARY_PATH") or (
-    "%USERPROFILE%/Desktop/apache-tomcat-10.1.26"
-    if os.environ.get("COMPUTERNAME") == "ARKG15"  # my laptop
-    else "/home/vixen-kite-celery/apache-tomcat-10.1.26"  # GCP server
+tomcat_path = os.getenv(
+    "SAFETO_BINARY_PATH",
+    (
+        "%USERPROFILE%/Desktop/apache-tomcat-10.1.26"
+        if is_my_laptop
+        else "/home/vixen-kite-celery/apache-tomcat-10.1.26"  # GCP server
+    ),
 )
 
 
 def main():
     # Sync with the codebase on GitHub
-    subprocess.run("git", "pull")
+    subprocess.run(args=("git", "pull"))
 
     # Check out the main branch
-    subprocess.run("git", "checkout", "main")
+    subprocess.run(args=("git", "checkout", "main"))
 
     # Gradle build
     os.chdir(backend_dir)
-    subprocess.run("./gradlew", "build")
+    subprocess.run(args=("gradlew.bat" if is_my_laptop else "./gradlew", "build"))
+    os.chdir(current_dir)
 
-    # Copy build result to deploy folder
-    shutil.copyfile(war_path, os.path.join(tomcat_path, "webapps"))
+    # Copy build result to the deploy folder
+    shutil.copyfile(war_path, os.path.join(tomcat_path, "webapps", "ROOT.war"))
 
 
 if __name__ == "__main__":
@@ -35,8 +42,8 @@ if __name__ == "__main__":
     try:
         main()
         status = "ok"
-        message = "updated to the latest version on GitHub; commit hash = {}".format(
-            subprocess.check_output(args=("git", "rev-parse", "HEAD")).decode().strip()
+        message = "Updated to the latest version on GitHub; commit hash = {}".format(
+            subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
         )
     except Exception as err:
         status = "error"
@@ -45,4 +52,4 @@ if __name__ == "__main__":
         with open(
             os.path.join(current_dir, "deploy.log"), "a", encoding="utf-8"
         ) as fout:
-            fout.write(f"{datetime.now().isoformat()} [{status}]: {message}")
+            fout.write(f"{datetime.now().isoformat()} [{status}]: {message}\n")
