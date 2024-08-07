6b6e482e6d5df7d2b66de7274a78a3852b24db16
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-08-06 23:03:23 -0400

    fix(.scripts): inline dict

--- .scripts/generate_database_secrets.py
+++ .scripts/generate_database_secrets.py
@@ -1,9 +1,16 @@
 import base64
 import os
-import tomllib
+
+import toml
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 output_path = os.path.join(current_dir, "database_secrets.toml")
+
+
+# Reference: <https://bit.ly/3SD0Eol>
+class InlineDict(dict, toml.decoder.InlineTableDict):
+    pass
+
 
 users = (
     "joe.fang@mail.utoronto.ca",
@@ -17,20 +24,28 @@ deployments = ("gcp@joefang.org",)
 authorized_users = []
 for user in users:
     authorized_users.append(
-        {
-            "user": user,
-            "token": base64.urlsafe_b64encode(os.urandom(36)).decode(),
-            "comment": "Purpose: Local Test",
-        }
+        InlineDict(
+            {
+                "user": user,
+                "token": base64.urlsafe_b64encode(os.urandom(36)).decode(),
+                "comment": "Purpose: Local Test",
+            }
+        )
     )
 for user in deployments:
     authorized_users.append(
-        {
-            "user": user,
-            "token": base64.urlsafe_b64encode(os.urandom(36)).decode(),
-            "comment": "Purpose: Deployment",
-        }
+        InlineDict(
+            {
+                "user": user,
+                "token": base64.urlsafe_b64encode(os.urandom(36)).decode(),
+                "comment": "Purpose: Deployment",
+            }
+        )
     )
 
 with open(output_path, "w", encoding="utf-8") as fout:
-    tomllib.dump({"env": {"AUTHORIZED_USERS": authorized_users}}, fout)
+    toml.dump(
+        {"AUTHORIZED_USERS": authorized_users},
+        fout,
+        encoder=toml.TomlPreserveInlineDictEncoder(),
+    )
