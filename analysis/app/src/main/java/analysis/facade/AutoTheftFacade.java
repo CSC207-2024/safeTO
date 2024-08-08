e3a0fa802fe17e64a7e31a52bbccaf2ba6bd0721
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-08-08 15:19:06 -0400

    fix(analysis/performance): investigate performance problems with analysis API
    Current result:
    > Task :app:run
    SLF4J(I): Connected with provider of type [org.slf4j.nop.NOPServiceProvider]
    Total aggregated data: 395140
    fetchData: 1.668
    Total aggregated data: 395140
    fetchData: 1.592
    getCrimeDataWithinRadiusPastYear: 3.007
    Total aggregated data: 395140
    fetchData: 1.428
    getCrimeDataWithinRadius: 2.562
    pastYearData.sort: 0.006
    allKnownData.sort: 0.0
    pastYearIncidents: 0.0
    allKnownIncidents: 0.0
    calculateAnnualAverageIncidents: 0.0
    calculatePoissonProbability: 0.0
    Auto Theft Result:
    {"pastYearIncidents":[{"occurDate":"2023-December-16","distance":64.23764572552567},{"occurDate":"2023-December-16","distance":64.23764572552567},{"occurDate":"2023-November-12","distance":64.23764572552567},{"occurDate":"2024-May-25","distance":186.89985905755003}],"allKnownIncidents":[{"occurDate":"2021-June-14","distance":82.88142166536086},{"occurDate":"2021-November-14","distance":64.23764572552567},{"occurDate":"2022-April-11","distance":64.23764572552567},{"occurDate":"2023-December-16","distance":64.23764572552567},{"occurDate":"2023-December-16","distance":64.23764572552567},{"occurDate":"2023-June-13","distance":155.0007847434158},{"occurDate":"2023-November-12","distance":64.23764572552567},{"occurDate":"2024-May-25","distance":186.89985905755003}],"probability":0.016563608480614334,"probabilityMessage":"Based on past data, within 200m of radius, there\u0027s a 1.6564% chance that auto thefts happen more than 5 time(s) within a year.","warning":"Safe to park here!","safeParkingSpots":[]}
    
    BUILD SUCCESSFUL in 11s
    2 actionable tasks: 2 executed

--- analysis/app/src/main/java/analysis/facade/AutoTheftFacade.java
+++ analysis/app/src/main/java/analysis/facade/AutoTheftFacade.java
@@ -13,7 +13,8 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * A facade class that provides a simplified interface to the auto theft analysis system.
+ * A facade class that provides a simplified interface to the auto theft
+ * analysis system.
  */
 public class AutoTheftFacade {
 
@@ -24,35 +25,38 @@ public class AutoTheftFacade {
         CrimeDataFetcher fetcher = new CrimeDataFetcher();
         CrimeDataConverter converter = new CrimeDataConverter();
         CrimeDataProcessor processor = new CrimeDataProcessor();
-        IncidentFetcherInterface<AutoTheftData> incidentFetcher = new AutoTheftIncidentFetcher(fetcher, converter, processor);
+        IncidentFetcherInterface<AutoTheftData> incidentFetcher = new AutoTheftIncidentFetcher(fetcher, converter,
+                processor);
         this.autoTheftCalculator = new AutoTheftCalculator(incidentFetcher);
         this.safeParkingLocationManager = SafeParkingLocationManager.getInstance();
     }
 
     public AutoTheftResult analyze(double latitude, double longitude, int radius, int threshold, int earliestYear) {
         long start = System.currentTimeMillis();
-        List<AutoTheftData> pastYearData = autoTheftCalculator.getCrimeDataWithinRadiusPastYear(latitude, longitude, radius);
+        List<AutoTheftData> pastYearData = autoTheftCalculator.getCrimeDataWithinRadiusPastYear(latitude, longitude,
+                radius);
         long curr = System.currentTimeMillis();
-        System.err.println("getCrimeDataWithinRadiusPastYear" + (curr - start) / 1000);
+        System.err.println("getCrimeDataWithinRadiusPastYear: " + (curr - start) / 1000.0);
 
         start = curr;
-        List<AutoTheftData> allKnownData = autoTheftCalculator.getCrimeDataWithinRadius(latitude, longitude, radius, earliestYear);
+        List<AutoTheftData> allKnownData = autoTheftCalculator.getCrimeDataWithinRadius(latitude, longitude, radius,
+                earliestYear);
         curr = System.currentTimeMillis();
-        System.err.println("getCrimeDataWithinRadius" + (curr - start) / 1000);
+        System.err.println("getCrimeDataWithinRadius: " + (curr - start) / 1000.0);
 
         start = curr;
         pastYearData.sort(Comparator.comparing(AutoTheftData::getOccYear)
                 .thenComparing(AutoTheftData::getOccMonth)
                 .thenComparing(AutoTheftData::getOccDay));
         curr = System.currentTimeMillis();
-        System.err.println("pastYearData.sort" + (curr - start) / 1000);
+        System.err.println("pastYearData.sort: " + (curr - start) / 1000.0);
 
         start = curr;
         allKnownData.sort(Comparator.comparing(AutoTheftData::getOccYear)
                 .thenComparing(AutoTheftData::getOccMonth)
                 .thenComparing(AutoTheftData::getOccDay));
         curr = System.currentTimeMillis();
-        System.err.println("allKnownData.sort" + (curr - start) / 1000);
+        System.err.println("allKnownData.sort: " + (curr - start) / 1000.0);
 
         start = curr;
         List<AutoTheftResult.Incident> pastYearIncidents = new ArrayList<>();
@@ -62,24 +66,37 @@ public class AutoTheftFacade {
             pastYearIncidents.add(new AutoTheftResult.Incident(occurDate, distance));
         }
         curr = System.currentTimeMillis();
-        System.err.println("pastYearIncidents" + (curr - start) / 1000);
+        System.err.println("pastYearIncidents: " + (curr - start) / 1000.0);
 
+        start = curr;
         List<AutoTheftResult.Incident> allKnownIncidents = new ArrayList<>();
         for (AutoTheftData data : allKnownData) {
             double distance = GeoUtils.calculateDistance(latitude, longitude, data.getLatitude(), data.getLongitude());
             String occurDate = data.getOccYear() + "-" + data.getOccMonth() + "-" + data.getOccDay();
             allKnownIncidents.add(new AutoTheftResult.Incident(occurDate, distance));
         }
+        curr = System.currentTimeMillis();
+        System.err.println("allKnownIncidents: " + (curr - start) / 1000.0);
 
+        start = curr;
         double lambda = autoTheftCalculator.calculateAnnualAverageIncidents(allKnownData);
-        double probability = autoTheftCalculator.calculatePoissonProbability(lambda, threshold);
+        curr = System.currentTimeMillis();
+        System.err.println("calculateAnnualAverageIncidents: " + (curr - start) / 1000.0);
 
-        String probabilityMessage = String.format("Based on past data, within %dm of radius, there's a %.4f%% chance that auto thefts happen more than %d time(s) within a year.", radius, probability * 100, threshold);
+        start = curr;
+        double probability = autoTheftCalculator.calculatePoissonProbability(lambda, threshold);
+        curr = System.currentTimeMillis();
+        System.err.println("calculatePoissonProbability: " + (curr - start) / 1000.0);
+
+        String probabilityMessage = String.format(
+                "Based on past data, within %dm of radius, there's a %.4f%% chance that auto thefts happen more than %d time(s) within a year.",
+                radius, probability * 100, threshold);
         String warning = probability > 0.15 ? "WARNING: Don't park here!" : "Safe to park here!";
 
         List<SafeParkingSpot> safeSpotsList = new ArrayList<>();
         if (probability > 0.15) {
-            List<Row> safeSpots = safeParkingLocationManager.getNearbySafeLocations(latitude, longitude, radius, 3, threshold);
+            List<Row> safeSpots = safeParkingLocationManager.getNearbySafeLocations(latitude, longitude, radius, 3,
+                    threshold);
             for (Row spot : safeSpots) {
                 double spotLat = spot.getDouble("Latitude");
                 double spotLon = spot.getDouble("Longitude");
@@ -89,10 +106,12 @@ public class AutoTheftFacade {
                 int spotThreshold = spot.getInt("Threshold");
 
                 double spotDistance = GeoUtils.calculateDistance(latitude, longitude, spotLat, spotLon);
-                safeSpotsList.add(new SafeParkingSpot(spotLat, spotLon, spotDistance, date, spotProbability, spotRadius, spotThreshold));
+                safeSpotsList.add(new SafeParkingSpot(spotLat, spotLon, spotDistance, date, spotProbability, spotRadius,
+                        spotThreshold));
             }
         }
 
-        return new AutoTheftResult(pastYearIncidents, allKnownIncidents, probability, probabilityMessage, warning, safeSpotsList);
+        return new AutoTheftResult(pastYearIncidents, allKnownIncidents, probability, probabilityMessage, warning,
+                safeSpotsList);
     }
 }
