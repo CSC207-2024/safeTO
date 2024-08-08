ece84371f25c9d4d5029bd9c47e4996e4bfa3058
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-08-08 16:00:18 -0400

    fix(analysis/CrimeDataFetcher): cache aggregated data

--- analysis/app/src/main/java/access/data/CrimeDataFetcher.java
+++ analysis/app/src/main/java/access/data/CrimeDataFetcher.java
@@ -1,6 +1,8 @@
 package access.data;
 
-import com.google.gson.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URLEncoder;
@@ -8,9 +10,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 /**
  * A public class implements InterfaceDataFetcher It is responsible for
@@ -20,6 +24,7 @@ public class CrimeDataFetcher implements InterfaceDataFetcher {
     private static final HttpClient httpClient = HttpClient.newHttpClient();
     private static final String BASE_API_URL = "https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Major_Crime_Indicators_Open_Data/FeatureServer/0/query?outFields=EVENT_UNIQUE_ID,OCC_DATE,OCC_YEAR,OCC_MONTH,OCC_DAY,OCC_DOY,OCC_DOW,OCC_HOUR,DIVISION,LOCATION_TYPE,PREMISES_TYPE,UCR_CODE,UCR_EXT,OFFENCE,MCI_CATEGORY,HOOD_140,NEIGHBOURHOOD_140,LONG_WGS84,LAT_WGS84,REPORT_DATE&outSR=4326&f=json";
     private static final String CACHE_DIR = CacheDirectory.getCacheDir();
+    private static final String AGGREGATED_DATA_FILENAME = "aggregated_data.json";
     private static final JsonArray cachedAggregatedData;
 
     static {
@@ -50,8 +55,21 @@ public class CrimeDataFetcher implements InterfaceDataFetcher {
     }
 
     private static JsonArray initializeAggregatedData() {
-        JsonArray aggregatedData = new JsonArray();
+        File cachedAggregateData = new File(CACHE_DIR, AGGREGATED_DATA_FILENAME);
         Gson gson = new Gson();
+        if (cachedAggregateData.exists()) {
+            System.err.println("Hit from Cached Aggregate Data: " + cachedAggregateData.getName());
+            try {
+                long start = System.currentTimeMillis();
+                JsonArray content = gson.fromJson(new FileReader(cachedAggregateData), JsonArray.class);
+                long curr = System.currentTimeMillis();
+                System.err.println("Total aggregated data: " + content.size());
+                System.err.println("initializeAggregatedData: " + (curr - start) / 1000.0);
+                return content;
+            } catch (Throwable e) {
+            }
+        }
+        JsonArray aggregatedData = new JsonArray();
 
         long start = System.currentTimeMillis();
         try {
@@ -69,6 +87,7 @@ public class CrimeDataFetcher implements InterfaceDataFetcher {
                     JsonObject jsonResponse;
 
                     if (cacheFile.exists()) {
+                        System.err.println("Hit from Cache: " + cacheFile.getName());
                         jsonResponse = gson.fromJson(new FileReader(cacheFile), JsonObject.class);
                     } else {
                         HttpResponse<String> response = httpClient.send(HttpRequest.newBuilder()
@@ -115,7 +134,12 @@ public class CrimeDataFetcher implements InterfaceDataFetcher {
 
         long curr = System.currentTimeMillis();
         System.err.println("Total aggregated data: " + aggregatedData.size());
-        System.err.println("fetchData: " + (curr - start) / 1000.0);
+        System.err.println("initializeAggregatedData: " + (curr - start) / 1000.0);
+
+        try (FileWriter writer = new FileWriter(cachedAggregateData)) {
+            gson.toJson(aggregatedData, writer);
+        } catch (Throwable e) {
+        }
         return aggregatedData;
     }
 }
